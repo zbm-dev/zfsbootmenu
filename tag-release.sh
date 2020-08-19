8680683deff44c77e19d511d45be436d6a0e435d
Andrew J. Hesford <ajh@sideband.org>
2020-08-19 00:13:20 -0400

    Edit docs for detail, grammar and clarity

--- tag-release.sh
+++ tag-release.sh
@@ -63,7 +63,7 @@ sed -i bin/generate-zbm -e "s/our \$VERSION.*/our \$VERSION = '${release}';/"
 pod2man pod/generate-zbm.5.pod -c "config.yaml" \
   -r "${release}" -s 5 -n generate-zbm > man/generate-zbm.5
 
-pod2man pod/zfsbootmenu.5.pod -c "ZFSBootMenu" \
+pod2man pod/zfsbootmenu.7.pod -c "ZFSBootMenu" \
   -r "${release}" -s 7 -n zfsbootmenu > man/zfsbootmenu.7
 
 pod2man bin/generate-zbm -c "generate-zbm" \
