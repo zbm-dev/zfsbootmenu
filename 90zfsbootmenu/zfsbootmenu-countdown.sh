075aa9cd8942f4950783389a33f7f4a58492385a
Zach Dykstra <dykstra.zachary@gmail.com>
2021-02-16 20:17:08 -0600

    Log which properties are unsupported

--- 90zfsbootmenu/zfsbootmenu-countdown.sh
+++ 90zfsbootmenu/zfsbootmenu-countdown.sh
@@ -60,6 +60,7 @@ fi
 unsupported=0
 while IFS=$'\t' read -r _pool _property; do
   if [[ "${_property}" =~ "unsupported@" ]]; then
+    zdebug "unsupported property: ${_property}"
     if ! grep -q "${_pool}" "${BASE}/degraded" >/dev/null 2>&1 ; then
       echo "${_pool}" >> "${BASE}/degraded"
     fi
