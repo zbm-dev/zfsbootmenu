c5e481d5d5c875c522b17fe6101e2a7150f227d3
Andrew J. Hesford <ajh@sideband.org>
2021-03-03 10:31:49 -0500

    Ensure ZFS modules are built on Debian

--- testing/helpers/chroot-debian.sh
+++ testing/helpers/chroot-debian.sh
@@ -10,7 +10,7 @@ deb http://deb.debian.org/debian buster-backports main contrib
 deb-src http://deb.debian.org/debian buster-backports main contrib
 EOF
 
-apt update
+apt-get update
 
 cat << EOF > /etc/apt/preferences.d/90_zfs
 Package: libnvpair1linux libuutil1linux libzfs2linux libzfslinux-dev libzpool2linux python3-pyzfs pyzfs-doc spl spl-dkms zfs-dkms zfs-dracut zfs-initramfs zfs-test zfsutils-linux zfsutils-linux-dev zfs-zed
@@ -18,12 +18,19 @@ Pin: release n=buster-backports
 Pin-Priority: 990
 EOF
 
-apt install --yes locales
-dpkg-reconfigure locales
+# Prevent terminal stupidity and interactive prompts
+export TERM=linux
+export DEBIAN_FRONTEND=noninteractive
+export DEBCONF_NONINTERACTIVE_SEEN=true
+
+apt-get install --yes locales
+dpkg-reconfigure -a -f noninteractive
 
 zpool set cachefile=/etc/zfs/zpool.cache ztest
 
-apt install --yes dpkg-dev linux-headers-amd64 linux-image-amd64 zfs-initramfs zfsutils-linux console-setup
+# Make sure the kernel is installed and configured before ZFS
+apt-get install --yes linux-headers-amd64 linux-image-amd64 console-setup
+apt-get install --yes zfs-dkms zfsutils-linux zfs-initramfs
 
 systemctl enable zfs.target
 systemctl enable zfs-import-cache
