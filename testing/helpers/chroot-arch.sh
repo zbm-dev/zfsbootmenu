15f54c557cff03d8b0e529a9d9ff722cc5f6eb88
Andrew J. Hesford <ajh@sideband.org>
2021-03-08 21:47:05 -0500

    Install Arch properly, use DKMS for ZFS kmods

--- testing/helpers/chroot-arch.sh
+++ testing/helpers/chroot-arch.sh
@@ -20,12 +20,45 @@ EOF
 # Set root password
 echo 'root:zfsbootmenu' | chpasswd -c SHA256
 
+# Space checks in pacman don't work right
+sed -e "/CheckSpace/d" -i /etc/pacman.conf
+
+pacman-key --init
+pacman-key --populate
+
+# Do this in two stages to avoid dracut providing the initramfs virtual
+pacman --noconfirm -Sy linux linux-headers mkinitcpio vi openssh \
+  fakeroot automake autoconf pkg-config gcc make libtool binutils curl
+pacman --noconfirm -Sy dkms git dracut fzf kexec-tools cpanminus
+
+# Install ZFS command-line utilities
+runuser -u nobody -- /bin/sh -c "cd /tmp && \
+  git clone --depth=1 https://aur.archlinux.org/zfs-utils.git && \
+  cd zfs-utils && MAKEFLAGS='-j4' makepkg --skippgpcheck"
+pacman -U --noconfirm /tmp/zfs-utils/*.pkg.*
+rm -rf /tmp/zfs-utils
+
+# Install ZFS DKMS module
+runuser -u nobody -- /bin/sh -c "cd /tmp && \
+  git clone --depth=1 https://aur.archlinux.org/zfs-dkms.git && \
+  cd zfs-dkms && MAKEFLAGS='-j4' makepkg --skippgpcheck"
+pacman -U --noconfirm /tmp/zfs-dkms/*.pkg.*
+rm -rf /tmp/zfs-dkms
+
 ZFILES="/etc/hostid"
 for keyfile in /etc/zfs/*.key; do
   [ -e "${keyfile}" ] && ZFILES="${ZFILES} ${keyfile}"
 done
 
 sed -e "/HOOKS=/s/fsck/zfs/" -e "/FILES=/s@)@${ZFILES})@" -i /etc/mkinitcpio.conf
+
+# The initcpio hook that ships with zfs-dkms do not support encryption
+# Use the hook from archzfs instead
+zfsutils="https://raw.githubusercontent.com/archzfs/archzfs/master/src/zfs-utils"
+cpiolib="/usr/lib/initcpio"
+curl -L -o "${cpiolib}/hooks/zfs" "${zfsutils}/zfs-utils.initcpio.hook"
+curl -L -o "${cpiolib}/install/zfs" "${zfsutils}/zfs-utils.initcpio.install"
+curl -L -o "${cpiolib}/install/zfsencryptssh" "${zfsutils}/zfs-utils.initcpio.zfsncrypt.sshinstall"
 
 # Arch doesn't play nicely with the pre-existing cache
 rm -f /etc/zfs/zpool.cache
@@ -42,3 +75,6 @@ if [ -x /root/network-systemd.sh ]; then
   /root/network-systemd.sh
   rm /root/network-systemd.sh
 fi
+
+# Make sure gpg-agent components die to avoid blocking pool export
+gpgconf --homedir /etc/pacman.d/gnupg --kill all
--- testing/helpers/install-arch.sh
+++ testing/helpers/install-arch.sh
@@ -24,9 +24,7 @@ mkdir -p "${PACROOT}"/{db,cache,gnupg,hooks}
 
 cat >> "${PACROOT}/pacman.conf" << EOF
 [options]
-DBPath = ${PACROOT}/db
 CacheDir = ${PACROOT}/cache
-LogFile = ${PACROOT}/pacman.log
 GPGDir = ${PACROOT}/gnupg
 HookDir = ${PACROOT}/hooks
 HoldPkg = pacman glibc
@@ -42,9 +40,6 @@ Include = ${PACROOT}/mirrorlist
 
 [community]
 Include = ${PACROOT}/mirrorlist
-
-[archzfs]
-Include = ${PACROOT}/zfsmirrors
 EOF
 
 cat >> "${PACROOT}/mirrorlist" << 'EOF'
@@ -54,43 +49,17 @@ Server = http://mirror.arizona.edu/archlinux/$repo/os/$arch
 Server = http://mirrors.rit.edu/archlinux/$repo/os/$arch
 EOF
 
-cat >> "${PACROOT}/zfsmirrors" << 'EOF'
-Server = http://archzfs.com/$repo/x86_64
-Server = http://mirror.sum7.eu/archlinux/archzfs/$repo/x86_64
-Server = https://mirror.biocrafting.net/archlinux/archzfs/$repo/x86_64
-Server = https://mirror.in.themindsmaze.com/archzfs/$repo/x86_64
-Server = https://zxcvfdsa.com/archzfs/$repo/$arch
-EOF
+"${PACROOT}/arch-install-scripts/pacstrap" \
+  -C "${PACROOT}/pacman.conf" "${CHROOT_MNT}" base
 
-pacman-key --config "${PACROOT}/pacman.conf" --init
-
-ARCHZFS_KEY=DDF7DB817396A49B2A2723F7403BD972F75D9D76
-pacman-key --config "${PACROOT}/pacman.conf" -r "${ARCHZFS_KEY}"
-pacman-key --config "${PACROOT}/pacman.conf" --lsign-key "${ARCHZFS_KEY}"
-
-pacstrap() {
-  yes | "${PACROOT}/arch-install-scripts/pacstrap" \
-    -C "${PACROOT}/pacman.conf" "${CHROOT_MNT}" "$@"
-}
-
-# This is done in two stages to avoid dracut satisfying initramfs
-pacstrap base archzfs-linux
-pacstrap openssh vi git dracut fzf kexec-tools cpanminus gcc make
+cp /etc/hostid "${CHROOT_MNT}/etc/"
+cp /etc/resolv.conf "${CHROOT_MNT}/etc/"
+cp "${PACROOT}/mirrorlist" "${CHROOT_MNT}/etc/pacman.d/"
 
 if [ -r "${ENCRYPT_KEYFILE}" ]; then
   mkdir -p "${CHROOT_MNT}/etc/zfs"
   cp "${ENCRYPT_KEYFILE}" "${CHROOT_MNT}/etc/zfs/"
 fi
-
-cp /etc/hostid "${CHROOT_MNT}/etc/"
-cp /etc/resolv.conf "${CHROOT_MNT}/etc/"
-cp "${PACROOT}/mirrorlist" "${CHROOT_MNT}/etc/pacman.d/"
-cp "${PACROOT}/zfsmirrors" "${CHROOT_MNT}/etc/pacman.d/"
-
-cat >> "${CHROOT_MNT}/etc/pacman.conf" << EOF
-[archzfs]
-Include = /etc/pacman.d/zfsmirrors
-EOF
 
 # Add network configuration script
 if [ -x ./helpers/network-systemd.sh ]; then
