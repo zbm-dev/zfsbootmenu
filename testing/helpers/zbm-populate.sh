f866bf958a33ffdaa95833e295a9959beaf0ffad
Andrew J. Hesford <ajh@sideband.org>
2021-03-10 10:20:22 -0500

    Use rootfs tarball and preinstall ZBM in Void testbeds

--- testing/helpers/zbm-populate.sh
+++ testing/helpers/zbm-populate.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-# Pre-populate ZFSBootMenu and all Perl dependencies
+# Pre-populate ZFSBootMenu
 ( cd / && git clone https://github.com/zbm-dev/zfsbootmenu.git )
-( cd /zfsbootmenu && cpanm --notest --installdeps . && make install )
+( cd /zfsbootmenu && make install )
+
+# Install perl dependencies if necessary
+if [ -z "${SKIP_PERL}" ]; then
+  ( cd /zfsbootmenu && cpanm --notest --installdeps . )
+fi
 
 # Adjust the configuration for convenient builds
 if [ -f /etc/zfsbootmenu/config.yaml ]; then
   sed -e 's/Versions:.*/Versions: false/' \
-	  -e 's/ManageImages:.*/ManageImages: true/' \
-	  -e 's@ImageDir:.*@ImageDir: /zfsbootmenu/build@' \
-	  -e '/BootMountPoint:/d' -i /etc/zfsbootmenu/config.yaml
+      -e 's/ManageImages:.*/ManageImages: true/' \
+      -e 's@ImageDir:.*@ImageDir: /zfsbootmenu/build@' \
+      -e '/BootMountPoint:/d' -i /etc/zfsbootmenu/config.yaml
 fi
 
+# Replace key parts with symlinks to reflect source changes
 rm -f /usr/bin/generate-zbm
 ln -s /zfsbootmenu/bin/generate-zbm /usr/bin/
 
