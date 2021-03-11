4ee933818ddd29f276f9672e69ba6c11849a7cc8
Zach Dykstra <dykstra.zachary@gmail.com>
2021-03-10 22:49:37 -0600

    Show if BE is encrypted, add path to chroot prompt

--- 90zfsbootmenu/zfs-chroot.sh
+++ 90zfsbootmenu/zfs-chroot.sh
@@ -43,7 +43,7 @@ if mountpoint="$( allow_rw=yes mount_zfs "${selected}" )"; then
     echo -e "$( colorize orange "${selected}") is mounted ${writemode}, /tmp is shared and read/write\n"
 
     # regardless of shell, set PS1
-    if ! env "PS1=$( colorize orange "${selected}") > " chroot "${mountpoint}" "${_SHELL}" "${chroot_extra}" ; then
+    if ! env "PS1=$( colorize orange "${selected}") \w > " chroot "${mountpoint}" "${_SHELL}" "${chroot_extra}" ; then
       zdebug "chroot ${selected}:${_SHELL} returned code $?"
     fi
   fi
