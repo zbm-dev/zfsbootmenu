fc83894a695a4969a9dc3042660c990d520ecc11
Zach Dykstra <dykstra.zachary@gmail.com>
2022-03-22 09:00:01 -0500

    Document scrolling keys in help viewer

--- releng/pod2help.sh
+++ releng/pod2help.sh
@@ -2,8 +2,8 @@
 set -e
 
 td="zfsbootmenu/help-files"
-for size in 54 94 134 ; do
-  rm "${td}/${size}"/*
+for size in 52 92 132 ; do
+  rm "${td}/${size}"/* >/dev/null 2>&1 || /bin/true
   for pod in docs/pod/online/*.pod docs/pod/zfsbootmenu.7.pod; do
     [ -d "${td}/${size}" ] || mkdir -p "${td}/${size}"
     file="$( basename "${pod}" )"
