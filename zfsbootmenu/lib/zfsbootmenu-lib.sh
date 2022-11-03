zbm-dev/zfsbootmenu

commit 3399f4bdc6ca70b5668b975f18fd718184023578

Move dataset mountpoint to $BASE/environments/
Creating a directory tree for zpools / datasets rooted in $BASE could
potentially lead to a conflict between one of the various files we
create and a pool name. Moving the root of this to $BASE/environments/
means that there can't be a conflict, since imported pool names must be
unique.

Additionally, a helper was added to return a string with the path to a
given filesystem on disk. This avoids manually constructing the path and
having to update those individually should this directory location ever
change.

zfsbootmenu/lib/zfsbootmenu-lib.sh
@@ -589,7 +589,7 @@ populate_be_list() {
   ret=1
   for fs in "${candidates[@]}"; do
     # Remove any existing cmdline cache
-    rm -f "${BASE}/${fs}/cmdline"
+    rm -f "$( be_location "${fs}" )/cmdline"
 
     # Unlock if necessary
     load_key "${fs}" || continue

zfsbootmenu/lib/zfsbootmenu-core.sh
@@ -240,7 +240,7 @@ mount_zfs() {
     return 1
   fi
 
-  mnt="${BASE}/${fs}/mnt"
+  mnt="$( be_location "${fs}" )/mnt"
   mkdir -p "${mnt}"
 
   # filesystems are readonly by default, but read-write mounts may be requested
@@ -782,7 +782,7 @@ find_be_kernels() {
 # returns: nothing
 
 select_kernel() {
-  local zfsbe specific_kernel kexec_args spec_kexec_args
+  local zfsbe bepath specific_kernel kexec_args spec_kexec_args
 
   zfsbe="${1}"
   if [ -z "${zfsbe}" ]; then
@@ -791,8 +791,10 @@ select_kernel() {
   fi
   zdebug "zfsbe set to ${zfsbe}"
 
+  bepath="$( be_location "${zfsbe}" )"
+
   # By default, select the last kernel entry
-  kexec_args="$( tail -1 "${BASE}/${zfsbe}/kernels" )"
+  kexec_args="$( tail -1 "${bepath}/kernels" )"
 
   # If a specific kernel is listed, prefer it when possible
   specific_kernel="$( zfs get -H -o value org.zfsbootmenu:kernel "${zfsbe}" )"
@@ -806,7 +808,7 @@ select_kernel() {
         kexec_args="${spec_kexec_args}"
         break
       fi
-    done <<<"$( tac "${BASE}/${zfsbe}/kernels" )"
+    done <<<"$( tac "${bepath}/kernels" )"
   fi
 
   zdebug "using kexec args: ${kexec_args}"
@@ -921,7 +923,7 @@ load_be_cmdline() {
   fi
   zdebug "fs set to ${fs}"
 
-  cache="${BASE}/${fs}/cmdline"
+  cache="$( be_location "${fs}" )/cmdline"
 
   if [ -r "${BASE}/cmdline" ]; then
     # Always prefer a user-entered KCL
@@ -1741,6 +1743,24 @@ load_key() {
   zdebug "prompting for passphrase for ${encroot}"
   zfs load-key -L prompt "${encroot}"
   return $?
+}
+
+# arg1: ZFS filesystem
+# prints: The base path to this filesystem for use by ZFSBootMenu functions, with out a trailing /
+# returns: nothing
+
+be_location() {
+  local fs beloc
+  fs="${1}"
+  if [ -z "${fs}" ]; then
+    zerror "fs is undefined"
+    return 1
+  fi
+  zdebug "fs set to ${fs}"
+
+  local beloc="${BASE}/environments/${fs}"
+  mkdir -p "${beloc}"
+  echo "${beloc}"
 }
 
 # arg1: ZFS filesystem

commit 73878560d4cfba118c172d30c2f3db20a1f23520

Fix kernel selection after introducing $BASE/environments
After commit 3399f4bdc6ca, ZBM no longer allows user to select
a kernel to boot, with the log error message:

ZFSBootMenu: kernel file zfsbootmenu/<pool and bootenv here>/kernels missing

That is due to that the path to the kernels list hasn't been updated
to use be_location() and still uses the old "zfsbootmenu/<pool>"
scheme. Doing `ln -s` from the recovery shell solves the problem.
Adjust that place accordingly to fix the issue.

Fixes: 3399f4bdc6ca ("Move dataset mountpoint to $BASE/environments/")
Signed-off-by: Alexander Lobakin <alobakin@pm.me>

zfsbootmenu/lib/zfsbootmenu-lib.sh
@@ -135,7 +135,7 @@ draw_kernel() {
     return 130
   fi
 
-  _kernels="${BASE}/${benv}/kernels"
+  _kernels="$( be_location "${benv}" )/kernels"
   if [ ! -r "${_kernels}" ] ; then
     zerror "kernel file ${_kernels} missing"
     return 130