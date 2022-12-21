d674d2f4f44c992605d5c3c6b35d7737541adfce
classabbyamp <dev@placeviolette.net>
2022-12-21 12:17:35 -0500

    docs: add favicon

--- docs/conf.py
+++ docs/conf.py
@@ -40,6 +40,7 @@ extlinks = {
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+html_favicon = '_static/favicon.ico'
 smartquotes = False
 html_logo = '_static/logo.svg'
 html_theme_options = {
