a3755e5b1e442247ed6b26f7f78f4bc44c291150
classabbyamp <dev@placeviolette.net>
2022-12-21 17:27:07 -0500

    releng/rst2help.sh: remove unhandled roles from docs before conversion

--- releng/rst2help.sh
+++ releng/rst2help.sh
@@ -6,8 +6,9 @@ for size in 52 92 132 ; do
   rm "${td}/${size}"/* >/dev/null 2>&1 || /bin/true
   for doc in docs/online/*.rst docs/man/zfsbootmenu.7.rst; do
     [ -d "${td}/${size}" ] || mkdir -p "${td}/${size}"
-    file="$( basename "${doc}" )"
+    file="$( basename -s .rst "${doc}" ).ansi"
     echo "Converting ${doc}"
-    COLUMNS="${size}" rst2ansi "${doc}" | sed '1s/ - \(.*\)$/\n\n\o033\[1m\1\o033\[0m/;s/\[3m/\[33m/g' > "${td}/${size}/${file}"
+    COLUMNS="${size}" rst2ansi <(sed -E 's/:(doc|manpage):`([^<`]+)( <[^>]+>)?`/`\2`/g' "${doc}") | \
+        sed '1s/ - \(.*\)$/\n\n\o033\[1m\1\o033\[0m/;s/\[3m/\[33m/g' > "${td}/${size}/${file}"
   done
 done
