ae519b4000bde75cff3bdd0c814912f7dbaa44ac
Zach Dykstra <dykstra.zachary@gmail.com>
2023-07-14 22:22:27 -0500

    release,recovery: add tools to images
    * Add ca-certs to recovery image, so cURL can connect over HTTPS
    * Add sgdisk to recovery image
    * Add zbm-kcl / zbm-kcl.8 documentation to release and recovery images
    
    Closes #400

--- releng/rst2help.sh
+++ releng/rst2help.sh
@@ -6,7 +6,7 @@ set -e
 td="zfsbootmenu/help-files"
 for size in 52 92 132 ; do
   rm "${td}/${size}"/* >/dev/null 2>&1 || /bin/true
-  for doc in docs/online/*.rst docs/man/zfsbootmenu.7.rst; do
+  for doc in docs/online/*.rst docs/man/zfsbootmenu.7.rst docs/man/zbm-kcl.8.rst; do
     [ -d "${td}/${size}" ] || mkdir -p "${td}/${size}"
     file="$( basename -s .rst "${doc}" ).ansi"
     echo "Converting ${doc} for ${size} columns"
