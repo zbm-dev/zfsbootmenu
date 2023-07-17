f72160d6034fe05860ef409da38c5cbdaac04c70
rdmitry0911 <rdmitry0911@gmail.com>
2023-07-17 17:30:23 +0300

    Update zfsbootmenu-core.sh to work with clevis-based zfs pool unlocking
    Provided a function for automatic unlocking of encrypted zfs pool using clevis and pcr_ids 1,4,5,7,9 of tpm2 module

--- zfsbootmenu/lib/zfsbootmenu-core.sh
+++ zfsbootmenu/lib/zfsbootmenu-core.sh
@@ -1652,11 +1652,173 @@ cache_key() {
   return $ret
 }
 
+###############
+### clevis hook
+###############
+
+Requirements:
+# - OTB clevis (full set) and optionally dropbear packages are embedded in zfsbootmenu
+# - latchset.clevis:decrypt=yes user property has to be added in advance to the encrypted dataset for automatic decryption
+# - latchset.clevis:netconf user property has to be added in advance to the encrypted dataset.
+#   The value of this property should be like this: "if:ip/mask:def. route:dns" Valid example: "eth0:10.7.6.22/24:10.7.6.1:8.8.8.8"
+#   This property is used to configure network for ssh accsess to ZBM. I use this way of passing net config params to a script to
+#   avoid rebuilding of ZBM for running on another host. In case there is no need to access ZBM via ssh this property is not needed
+# - /boot should reside inside the encrypted dataset
+# - keylocation of the encrypted dataset should be set to file:///some/file Valid example: file:///etc/zfs/rpool.key and this file
+#   should be embedded to initramfs of the target system. It is safe as initramfs  is located in encrypted /boot directory
+#
+# The logic of the module is this:
+# - Before asking the passphrase in zfsbootmenu this module checks if the volume is eligable for automatic unlocking
+# - if yes, it
+#       1. Trys to decrypt the passphrase stored in a special property in encrypted format. The script uses clevis and tpm2 for that
+#       2. In case of failure it asks the user a passphrase and check if it is valid
+#       3. Valid passphrase is stored in clear text in keylocation (in fact in RAM) and in encrypted format bound to tpm2 in a special
+#          user property latchset.clevis:jwe of the encrypted dataset for next boots
+# - Then module returns the control back to ZBM
+#
+# arg1: ZFS filesystem
+# prints: nothing
+# asks: passphrase
+# returns: 0 on success, 1 on failure
+#
+
 # arg1: ZFS filesystem
 # prints: nothing
 # returns: 0 on success, 1 on failure
 #
 # NOTE: this function should *not* be called from a subshell
+
+get_fs_value()
+{
+        fs="$1"
+        value=$2
+
+        zfs get -H -ovalue "$value" "$fs" 2> /dev/null
+}
+
+reseal_data_set()
+{
+  read -p "We have autodecrypt flag set to on, however $1 can't be unlocked. Would you like to reseal the password [yes/no] " SRESEAL
+  seq=3
+  if [[ "$SRESEAL" == "yes" ]]; then
+    while [[ "$seq" -gt 0 ]]
+    do
+      read -s -p  "Type the password, please, to unlock $1. You have $seq attempts left : " PASS
+      echo -n "$PASS" | zfs load-key -n -L prompt "$1" >&2
+      res=$?
+      if [[ "$res" == "0" ]]; then
+        seq=0
+      else
+        seq=$((seq - 1))
+        PASS=""
+      fi
+    done
+    echo "$PASS"
+    return 0
+  else
+    # In fact we don't want to reseal
+    echo ""
+    return 1
+  fi
+}
+
+load_key_clevis() {
+
+  dataset_for_clevis_unlock="$1"
+  zdebug "Processing dataset $dataset_for_clevis_unlock"
+
+  # We need to setup network for remote access
+  # We use latchset.clevis:netconf property in encrypted dataset to store net config
+
+  if [[ ! -f "/tmp/clevis_net" ]]; then # Reconfigure network only once
+    CLEVIS_NET="$(get_fs_value "${dataset_for_clevis_unlock}" "latchset.clevis:netconf")"
+    if [[ "$CLEVIS_NET" != "-" ]]; then
+      # We have net config. Reconfigure network
+      : > /tmp/clevis_net
+      IFS=':' read -r -a netconf <<< "$CLEVIS_NET"
+      dev="${netconf[0]}"
+      ip="${netconf[1]}"
+      dr="${netconf[2]}"
+      dns="${netconf[3]}"
+      zdebug "Reconfigure network: dev: $dev ip: $ip default route: $dr dns: $dns"
+      ip addr add "$ip" brd + dev "$dev"
+      ip route add default via "$dr"
+      echo "nameserver $dns" > /etc/resolv.conf
+    fi
+  fi
+
+
+  CLEVIS_CHECK="$(zfs get -H -p -o value latchset.clevis:decrypt -s local $dataset_for_clevis_unlock)"
+  if [[ "$CLEVIS_CHECK" == "yes" ]]; then
+    zdebug "Found dataset for clevis unlocking: $dataset_for_clevis_unlock"
+    KEYLOCATION="$(get_fs_value "${dataset_for_clevis_unlock}" keylocation)"
+    if [[ -z "$(echo "$KEYLOCATION"|grep file)" ]]; then
+        # That's not us
+        zdebug "Warning: keylocation is not file while clevis unlock is set for dataset $dataset_for_clevis_unlock"
+        return 0
+    fi
+    # We suppose the keylocation has value in format file:///something/key
+    KEYLOCATION="${KEYLOCATION:7}"
+    KEYSTATUS="$(zfs get -H -p -o value keystatus -s none $dataset_for_clevis_unlock)"
+    if [[ "$KEYSTATUS" == "unavailable" ]]; then
+        # Prepare the key with password for unlocking in right place
+        mkdir -p "$(dirname "$KEYLOCATION")"
+        JWE="$(zfs get -H -p -o value latchset.clevis:jwe -s local $dataset_for_clevis_unlock)"
+        echo "$JWE" | clevis decrypt >"$KEYLOCATION"
+        cat "$KEYLOCATION" | zfs load-key -n $dataset_for_clevis_unlock
+        if [[ "$?" == "0" ]]; then
+          # We are fine
+          return 0
+        else
+          # We have autodecrypt flag set to on, however dataset can't be unlocked. Offer resealing the password
+          RESEAL="$(reseal_data_set "$dataset_for_clevis_unlock")"
+          if [[ "$RESEAL" != "" ]]; then
+            # We are fine
+            echo "$RESEAL"|clevis encrypt tpm2 '{"pcr_ids":"1,4,5,7,9","pcr_bank":"sha256"}' > /tmp/clevis_zfs.jwe
+            zdebug "Try to store correct jwe in $dataset_for_clevis_unlock"
+
+            # We need the pool to be writable
+            pool=${dataset_for_clevis_unlock%/*}
+            ro_state="$(zpool get -H -p -o value readonly "$pool")"
+            if [[ "$ro_state" = "on" ]]; then
+                zpool export "$pool"
+                zpool import -o readonly=off "$pool"
+                zfs set latchset.clevis:jwe="$(cat /tmp/clevis_zfs.jwe)" "$dataset_for_clevis_unlock"
+                zpool export "$pool"
+                zpool import -o readonly=on "$pool"
+            else
+                zfs set latchset.clevis:jwe="$(cat /tmp/clevis_zfs.jwe)" "$dataset_for_clevis_unlock"
+            fi
+
+            # check if we are fine
+            jwe_check="$(zfs get -H -p -o value latchset.clevis:jwe -s local $dataset_for_clevis_unlock)"
+            echo "$jwe_check" | clevis decrypt | zfs load-key -n -L prompt "$dataset_for_clevis_unlock"
+            if [[ "$?" != "0" ]]; then
+              # We failed. The jwe is not correctly stored in dataset
+              zdebug "Warning: We failed. The jwe is not correctly stored in dataset $dataset_for_clevis_unlock"
+              return 1
+            fi
+            echo "$RESEAL" >"$KEYLOCATION"
+            return 0
+          else
+            # We don't want to reseal
+            return 1
+          fi
+        fi
+    else
+      zdebug "This is strange. Without our help the key is available. Probably keyfile was by mistake put into ZBM initramfs. Plese recheck ZBM configuration"
+      return 1
+    fi
+  else
+    #  That's not us
+    zdebug "Flag for automatic decryption latchset.clevis:decrypt is not set for dataset $dataset_for_clevis_unlock"
+    return 0
+  fi
+}
+
+######################
+### end of clevis hook
+######################
 
 load_key() {
   local fs encroot key keypath keyformat keylocation keysource
@@ -1693,6 +1855,17 @@ load_key() {
     zfs load-key -L prompt "${encroot}"
     return $?
   fi
+
+  ###############
+  ### clevis hook
+  ###############
+
+  load_key_clevis "${encroot}"
+
+  ######################
+  ### end of clevis hook
+  ######################
+
 
   # Strip file path, relative to root
   key="${keylocation#file://}"
